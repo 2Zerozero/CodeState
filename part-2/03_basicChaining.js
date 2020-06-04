27af8809f5ccbcc4c312bde663b603f3557292b2
Lee Hoyong <igotoweb@gmail.com>
2020-06-04 16:34:44 +0900

    fix a bug on basicChaining

--- part-2/03_basicChaining.js
+++ part-2/03_basicChaining.js
@@ -1,16 +1,18 @@
+const path = require('path');
+
 const { getDataFromFilePromise } = require('./02_promiseConstructor');
 
+const user1Path = path.join(__dirname, 'files/user1.json');
+const user2Path = path.join(__dirname, 'files/user2.json');
+
 const readAllUsersChaining = () => {
-  return getDataFromFilePromise('files/user1.json')
+  return getDataFromFilePromise(user1Path)
     .then(user1 => {
-      return getDataFromFilePromise('files/user2.json').then(user2 => {
+      return getDataFromFilePromise(user2Path).then(user2 => {
         return '[' + user1 + ',' + user2 + ']';
       });
     })
     .then(text => JSON.parse(text))
-    .then(json => {
-      console.log(json);
-    });
 }
 
 readAllUsersChaining();
