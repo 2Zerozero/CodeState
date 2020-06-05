c59a4eec79fa682eabf51faac9cbbe7b5be2ec3a
Lee Hoyong <igotoweb@gmail.com>
2020-06-05 13:51:20 +0900

    add reference code callback version get request

---
+++ part-3/01_callBack.js
@@ -0,0 +1,26 @@
+const https = require('https')
+
+const getBodyFromGetRequest = function (url, callback) {
+  try {
+    https.get(url, (res) => {
+      let body = ''
+      res.on('data', chunk => {
+        body = body + chunk;
+      });
+
+      res.on('end', () => {
+        body = JSON.parse(body.toString());
+        callback(null, body);
+      });
+
+    }).on('error', err => {
+      callback(err, null);
+    })
+  } catch (err) {
+    callback(err, null);
+  }
+};
+
+module.exports = {
+  getBodyFromGetRequest
+};
