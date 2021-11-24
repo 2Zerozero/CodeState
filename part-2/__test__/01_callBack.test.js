74239db7d10a80be80d13dfeaca6f06b2dc2e46f
Hong Shik Branden Kim <kimploo@gmail.com>
2021-11-24 22:23:04 +0900

    merge

--- part-2/__test__/01_callBack.test.js
+++ part-2/__test__/01_callBack.test.js
@@ -1,7 +1,5 @@
 const path = require('path');
-const {
-  getDataFromFile
-} = require("../01_callBack");
+const { getDataFromFile } = require('../01_callBack');
 
 const jsonPath = path.join(__dirname, '..', 'files/user1.json');
 const user1txt = `{
@@ -11,26 +9,26 @@ const user1txt = `{
   "company": {
     "name": "코드스테이츠"
   }
-}`
+}`;
 
-describe("callback Test", () => {
-  describe("getDataFromFile", () => {
-    test("파일을 읽고 나서 callback이 실행되어야 합니다", done => {
+describe('callback Test', () => {
+  describe('getDataFromFile', () => {
+    test('파일을 읽고 나서 callback이 실행되어야 합니다', (done) => {
       getDataFromFile(jsonPath, () => {
         expect(true).toBeTruthy();
         done();
       });
     });
 
-    test("에러가 발생할 경우, callback 첫번째 인자에 에러 객체가 전달되어야 합니다", done => {
+    test('에러가 발생할 경우, callback 첫번째 인자에 에러 객체가 전달되어야 합니다', (done) => {
       getDataFromFile('nonexist', (err, data) => {
-        expect(err.code).toBe("ENOENT");
+        expect(err.code).toBe('ENOENT');
         expect(data).toBeNull();
         done();
       });
     });
 
-    test("callback 두번째 인자에 파일 내용이 전달되어야 합니다", done => {
+    test('callback 두번째 인자에 파일 내용이 전달되어야 합니다', (done) => {
       getDataFromFile(jsonPath, (err, data) => {
         expect(err).toBeNull();
         expect(data).toBe(user1txt);
