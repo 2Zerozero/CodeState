a435d13bee0a75af549d44b5cc83813a245da8c0
민제경 <71182382+Jekyung-Min@users.noreply.github.com>
2022-05-30 23:52:17 +0900

    fix: 중복된 변수 제거
    remove newURL, weatherURL

--- part-3/02_promiseAll.js
+++ part-3/02_promiseAll.js
@@ -1,6 +1,3 @@
-const newsURL = 'http://localhost:4999/data/latestNews';
-const weatherURL = 'http://localhost:4999/data/weather';
-
 function getNewsAndWeatherAll() {
   // TODO: Promise.all을 이용해 작성합니다
 }
