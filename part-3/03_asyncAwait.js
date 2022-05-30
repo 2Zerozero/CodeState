c938111e7520e3e3a4e9b081232b55778895a777
민제경 <71182382+Jekyung-Min@users.noreply.github.com>
2022-05-30 23:53:08 +0900

    fix: 중복된 변수 제거
    newsURL, weatherURL

--- part-3/03_asyncAwait.js
+++ part-3/03_asyncAwait.js
@@ -1,6 +1,3 @@
-const newsURL = 'http://localhost:4999/data/latestNews';
-const weatherURL = 'http://localhost:4999/data/weather';
-
 async function getNewsAndWeatherAsync() {
   // TODO: async/await 키워드를 이용해 작성합니다
 }
