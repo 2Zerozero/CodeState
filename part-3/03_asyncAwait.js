828f5458eabab500fb920463423255cf96021c54
민제경 <71182382+Jekyung-Min@users.noreply.github.com>
2022-05-30 23:56:39 +0900

    Update 03_asyncAwait.js
    fix: 중복된 newsURL, weatherURL 삭제

--- part-3/03_asyncAwait.js
+++ part-3/03_asyncAwait.js
@@ -1,6 +1,3 @@
-var newsURL = 'http://localhost:4999/data/latestNews';
-var weatherURL = 'http://localhost:4999/data/weather';
-
 async function getNewsAndWeatherAsync() {
 
   let json1 = await fetch(newsURL).then(resp => resp.json());
