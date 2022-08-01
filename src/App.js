1f174f7a1a562b8a1d53da7bd71381a82236e185
Citysquirrel <86960007+Citysquirrel@users.noreply.github.com>
2022-08-01 16:21:26 +0900

    chore: add import global style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import './global-style.css';
 // TODO - react-router-dom을 설치 후, import 구문을 이용하여 BrowserRouter, Routes, Route 컴포넌트를 불러오세요.
 
 import Sidebar from './Sidebar';
@@ -10,15 +11,15 @@ const App = () => {
   return (
     <div>
       {/* TODO - BrowserRouter 컴포넌트를 작성합니다. */}
-        <div className="App">
-          <main>
-            <Sidebar />
-            <section className="features">
-              {/* TODO - Routes와 Route 컴포넌트를 이용하여 경로(path)를 설정하고 Tweets, Mypage, About 컴포넌트를 연결합니다. */}
-              <Tweets />
-            </section>
-          </main>
-        </div>
+      <div className="App">
+        <main>
+          <Sidebar />
+          <section className="features">
+            {/* TODO - Routes와 Route 컴포넌트를 이용하여 경로(path)를 설정하고 Tweets, Mypage, About 컴포넌트를 연결합니다. */}
+            <Tweets />
+          </section>
+        </main>
+      </div>
     </div>
   );
 };
