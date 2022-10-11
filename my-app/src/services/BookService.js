25d4f9f4ce68aad22c5ad7b55894cb260a6d4927
정해림 <maestro@haerimjeong.local>
2022-10-11 15:53:14 +0900

    data 전송 부분 수정

--- my-app/src/services/BookService.js
+++ my-app/src/services/BookService.js
@@ -9,7 +9,7 @@ export const createBook = async (data) => {
     const response = await fetch('http://localhost:3080/api/book', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({todo: data})
+        body: JSON.stringify({book: data})
       })
     return await response.json();
 }
